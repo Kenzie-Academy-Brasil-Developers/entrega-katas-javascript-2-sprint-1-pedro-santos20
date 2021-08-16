da3ac2482e26be514a690e724413e96d528365a1
pedro santos <ps3979187@gmail.com>
2021-08-16 10:16:09 -0300

    Feito as devidas funções da entrega

--- katas2.js
+++ katas2.js
@@ -1,26 +1,39 @@
 // comece a criar a sua função add na linha abaixo
-
+function add(x, y) {
+    return x + y
+}
 
 // descomente a linha seguinte para testar sua função
 // console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
 // comece a criar a sua função multiply na linha abaixo
-
+function multiply(a, b) {
+    return a * b
+}
 
 // descomente a linha seguinte para testar sua função
 // console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
-
+function power(x, n) {
+    return Math.pow(x, n)
+}
 
 // descomente a linha seguinte para testar sua função
 // console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
 // comece a criar a sua função factorial na linha abaixo
-
+function factorial(z) {
+    let result = z;
+    let tentativa = z - 1;
+    for (let i = tentativa; i > 1; i--) {
+        result *= i;
+    }
+    return result
+}
 
 // descomente a linha seguinte para testar sua função
 // console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
