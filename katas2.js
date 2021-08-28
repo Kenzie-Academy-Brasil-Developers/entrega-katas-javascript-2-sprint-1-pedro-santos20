435fbfc93fbb5a1ca52ed3310530e4392e97f009
pedro santos <ps3979187@gmail.com>
2021-08-28 00:27:50 -0300

    Modificações no arquivo js

--- katas2.js
+++ katas2.js
@@ -9,30 +9,33 @@ function add(x, y) {
 
 // comece a criar a sua função multiply na linha abaixo
 function multiply(a, b) {
-    return a * b
+    let mult = 0;
+    for (i = 0; i < b; i++) {
+        mult = add(mult, a)
+    }
+    return mult
 }
 
 // descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
 function power(x, n) {
-    return Math.pow(x, n)
+    let resultado = 1;
+    for (i = 0; i < n; i++) {
+        resultado = multiply(n, resultado)
+    }
+    return resultado
 }
 
 // descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+console.assert(power(3, 4) === 81, ('A função power não está funcionando como esperado'));
 
 
 // comece a criar a sua função factorial na linha abaixo
 function factorial(z) {
-    let result = z;
-    let tentativa = z - 1;
-    for (let i = tentativa; i > 1; i--) {
-        result *= i;
-    }
-    return result
+
 }
 
 // descomente a linha seguinte para testar sua função
